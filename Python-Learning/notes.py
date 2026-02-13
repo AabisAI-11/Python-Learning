4a37a1faaf5c8b02c6f7090e92a9fe0aff739b00
Aabis-future11 <aabgeenaabshar@gmail.com>
2026-02-13 19:30:51 +0530

    Added File Handling Project folder properly

--- Python-Learning/notes.py
+++ Python-Learning/notes.py
@@ -7,6 +7,28 @@
 #>this is the reason why it returns ordered values 
 #keys are index for accessing values in a dictionary
 
+#EXCEPTION HANDLING:
+'''marks=int(input("enter marks:"))
+try:
+    if marks>100 or marks<0:
+       raise ValueError("your marks must be between 0 to 100")
+    else:
+       print("your result is evaluated")
+except Exception as error:
+   print(f"there is an error due to {error}")
+finally:
+   print("no matter what is happening")
+print("completed")'''
+
+r=open('nani.py',"x")
+r.write("Lets start something new")# W IS KNOWN FOR OVERWRITING.
+
+
+
+
+
+
+
 
 
 
